9f77561511f0e3597da7e6af8c2ee9d22b2b5831
DeYing Zhang <legendzdy@dingtalk.com>
2024-05-07 14:38:19 +0000

    chore: env

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,11 @@
     <el-button type="primary" size="default" icon="Plus">主要按钮</el-button>
     <el-button type="success" size="small" icon="Edit">编辑按钮</el-button>
     <el-button type="danger" size="default" icon="Delete">删除按钮</el-button>
-
-
   </div>
 </template>
 
 <script setup lang="ts">
-import {Plus, Edit, Delete} from "@element-plus/icons-vue"
+import { Plus, Edit, Delete } from '@element-plus/icons-vue'
 </script>
-
 
 <style scoped></style>
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import App from '@/App.vue'
 // 引入element-plus插件与样式
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+// 配置element-plus国际化
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
+
 // 获取应用实例对象
 const app = createApp(App)
 // 安装element-plus插件
-app.use(ElementPlus)
+app.use(ElementPlus,{
+    locale: zhCn //element-plus语言包国际化配置
+})
+
+console.log(import.meta.env)
 // 挂载应用挂载到挂载点上
 app.mount('#app')
