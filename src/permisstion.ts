72c3ac8e4a9316d915811529647dd0c7ff5b0044
DeYing Zhang <legendzdy@dingtalk.com>
2024-05-28 14:25:11 +0000

    feat: day20

---
+++ src/permisstion.ts
@@ -0,0 +1,17 @@
+//路由鉴权，项目中路由能不能被访问的权限控制在这里写
+import router from '@/router'
+
+//全局路由前置守卫
+router.beforeEach((to, from, next) => {
+  // console.log(to.meta.roles)
+  // console.log(store.getters.roles)
+  // if (to.meta.roles && !store.getters.roles.includes(to.meta.roles)) {
+  //   next('/403')
+  // } else {
+  //   next()
+  // }
+  next()
+})
+
+
+//全局路由后置守卫
