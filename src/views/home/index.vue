f7fb12162b78198bca4c0b1145971840e006ab48
DeYing Zhang <legendzdy@dingtalk.com>
2024-05-29 14:34:43 +0000

    feat: day21

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,21 +1,25 @@
 <template>
   <div>
     <h1>我是一级路由展示登录成功以后的页面 {{ userStore.username }}</h1>
+    <div ref="htmlContainer"></div>
   </div>
 </template>
 
 <script setup lang="ts">
 //引入组合式 API函数生命周期
-import { onMounted } from 'vue'
+import { ref, onMounted } from 'vue'
 import useUserStore from '@/store/modules/user'
+import htmlContent from '@/components/HtmlContent/index.vue'
 
 const userStore = useUserStore()
+const htmlContainer = ref("")
 
 // 页面加载完成后执行
 onMounted(() => {
   console.log('页面加载完成')
   // 获取用户信息
   userStore.userInfo()
+  htmlContainer.value.innerHTML = htmlContent
 })
 </script>
 
