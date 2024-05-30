fc924d96c7b9663181583daae48dc88a287bc421
DeYing Zhang <legendzdy@dingtalk.com>
2024-05-30 14:22:49 +0000

    feat: day22

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,7 +1,6 @@
 <template>
-  <div>
-    <h1>我是一级路由展示登录成功以后的页面 {{ userStore.username }}</h1>
-    <div ref="htmlContainer"></div>
+  <div class="home-container">
+    <iframe src="_book/index.html" ref="iframe" width="50%" height="100px"></iframe>
   </div>
 </template>
 
@@ -9,18 +8,21 @@
 //引入组合式 API函数生命周期
 import { ref, onMounted } from 'vue'
 import useUserStore from '@/store/modules/user'
-import htmlContent from '@/components/HtmlContent/index.vue'
 
 const userStore = useUserStore()
-const htmlContainer = ref("")
+const htmlContainer = ref('')
 
 // 页面加载完成后执行
 onMounted(() => {
   console.log('页面加载完成')
   // 获取用户信息
   userStore.userInfo()
-  htmlContainer.value.innerHTML = htmlContent
 })
 </script>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.home-container {
+  height: 100px;
+  width: 100%;
+}
+</style>
